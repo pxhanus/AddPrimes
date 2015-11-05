30f8f75dbbc5b61766bdc9fba4bc7eaa103c1083
pxhanus <pxhanus@gmail.com>
2015-11-05 14:53:05 -0600

    Initial commit

---
+++ AddPrimes/Form1.Designer.vb
@@ -0,0 +1,106 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txtNum1 = New System.Windows.Forms.TextBox()
+        Me.txtNum2 = New System.Windows.Forms.TextBox()
+        Me.lblAns = New System.Windows.Forms.Label()
+        Me.btnAddPrimes = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(13, 23)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(92, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Enter first number:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(13, 64)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(111, 13)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Enter second number:"
+        '
+        'txtNum1
+        '
+        Me.txtNum1.Location = New System.Drawing.Point(130, 20)
+        Me.txtNum1.Name = "txtNum1"
+        Me.txtNum1.Size = New System.Drawing.Size(100, 20)
+        Me.txtNum1.TabIndex = 2
+        '
+        'txtNum2
+        '
+        Me.txtNum2.Location = New System.Drawing.Point(130, 61)
+        Me.txtNum2.Name = "txtNum2"
+        Me.txtNum2.Size = New System.Drawing.Size(100, 20)
+        Me.txtNum2.TabIndex = 3
+        '
+        'lblAns
+        '
+        Me.lblAns.Location = New System.Drawing.Point(12, 95)
+        Me.lblAns.Name = "lblAns"
+        Me.lblAns.Size = New System.Drawing.Size(218, 28)
+        Me.lblAns.TabIndex = 4
+        Me.lblAns.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'btnAddPrimes
+        '
+        Me.btnAddPrimes.Location = New System.Drawing.Point(84, 138)
+        Me.btnAddPrimes.Name = "btnAddPrimes"
+        Me.btnAddPrimes.Size = New System.Drawing.Size(75, 23)
+        Me.btnAddPrimes.TabIndex = 5
+        Me.btnAddPrimes.Text = "Button1"
+        Me.btnAddPrimes.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(242, 173)
+        Me.Controls.Add(Me.btnAddPrimes)
+        Me.Controls.Add(Me.lblAns)
+        Me.Controls.Add(Me.txtNum2)
+        Me.Controls.Add(Me.txtNum1)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents txtNum1 As System.Windows.Forms.TextBox
+    Friend WithEvents txtNum2 As System.Windows.Forms.TextBox
+    Friend WithEvents lblAns As System.Windows.Forms.Label
+    Friend WithEvents btnAddPrimes As System.Windows.Forms.Button
+
+End Class
---
+++ AddPrimes/Form1.vb
@@ -0,0 +1,36 @@
+﻿Public Class Form1
+
+    Private Sub btnAddPrimes_Click(sender As Object, e As EventArgs) Handles btnAddPrimes.Click
+        Dim num1 As Integer = Val(Me.txtNum1.Text)
+        Dim num2 As Integer = Val(Me.txtNum2.Text)
+        Dim isPrime As Boolean = False
+
+        Call IsSumPrime(num1, num2, isPrime)
+
+        If Not isPrime Then
+            Me.lblAns.Text = "The sum is not prime"
+        Else
+            Me.lblAns.Text = "The sum is prime"
+        End If
+    End Sub
+    '****************************************************************
+    'IsSumPrime - returns true if num1 + num2 is prime and false otherwise
+    '
+    'pre:  num1 and num2 should primes
+    'post: true or false is returned in prime
+    '****************************************************************
+    Sub IsSumPrime(ByVal num1Dif As Integer, ByVal num2Diff As Integer, ByRef prime As Boolean)
+        Dim sum As Integer = num1Dif + num2Diff
+        Dim div As Integer = 2
+        Do While sum Mod div <> 0
+            div += 1
+        Loop
+
+        If sum = div Then
+            prime = True
+        Else
+            prime = False
+        End If
+    End Sub
+
+End Class
